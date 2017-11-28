91f07b6574c6076191c494176fbc06ca413a2e36
jmlopez2 <martin.lopez@infor.com>
2017-11-28 12:33:41 -0300

    first commit

---
+++ src/Jumbotron.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Jumbotron extends Component {
+  render() {
+    return (
+      <div>
+        <p>My name is martin</p>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
